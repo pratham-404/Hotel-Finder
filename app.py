363bf1fb8d4e75d2eb65c25fda8757d05c796f6e
Pratham Loya <prathamloya12345@gmail.com>
2023-04-04 13:32:19 +0530

    added waitress to run app in production

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
-from asyncio.windows_events import NULL
 from flask import Flask, jsonify, render_template, request
 from scripts.min import Min
 from scripts.range import Range
 import json
 
-app = Flask(__name__, static_url_path='/static')
+def create_app(): 
+    app = Flask(__name__, static_url_path='/static')
+    
+    @app.route("/")
+    def home():
+        return render_template('index.html', title="Home")
 
-@app.route("/")
-def home():
-    return render_template('index.html', title="Home")
+    @app.route('/search', methods=['GET'])
+    def search():
+        args = request.args
+        address = args.get("location", default="", type=str)
+        distance = args.get("radius", default=0, type=int)
 
-@app.route('/search', methods=['GET'])
-def search():
-    args = request.args
-    address = args.get("location", default="", type=str)
-    distance = args.get("radius", default=0, type=int)
+        if address != "":
+            if distance == 0: result = Min(address)
+            else: result = Range(address, distance)
+            return json.dumps(result.__dict__,indent=4)
+        else:
+            return "Invalid Address"
+        
+    return app
 
-    if address != "":
-        if distance == 0: result = Min(address)
-        else: result = Range(address, distance)
-        return json.dumps(result.__dict__,indent=4)
-    else:
-        return "Invalid Address"
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
-else:
-    gunicorn_app = app()
+    app.run(debug=True)
